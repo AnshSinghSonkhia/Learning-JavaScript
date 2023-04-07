eb22b2d21ee3861f7f5880fa2130a899d6a5070c
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-07 17:10:31 +0530

    Create Math floor function.js

---
+++ Math floor function.js
@@ -0,0 +1,7 @@
+// This function will take 2.2598223 and return 2. The function will round a number down to the nearest integer.
+// For example if we had the number, 2.9999, the function will round this input down to 2.
+
+function getFloor(x) {
+    value = Math.floor(x);
+    return value;
+}
