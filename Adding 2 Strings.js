d88f4ce6e66da3bb2275d38d31275aca811e4838
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-07 17:40:53 +0530

    Update Adding 2 Strings.js

--- Adding 2 Strings.js
+++ Adding 2 Strings.js
@@ -17,3 +17,21 @@ function scream(n) {
 
 scream(5); // "aaaaa"
 scream(10); // "aaaaaaaaaa"
+
+
+// Let's modify our function to return a scream alternating between lower and capital case.
+
+function scream(n) {
+    let str = "";
+
+    for(let i = 0; i<n ; i++){
+
+        if(i % 2 === 0){
+            str += "a";
+        } else {
+            str += "A";
+        }
+    }
+
+    return str;
+}
