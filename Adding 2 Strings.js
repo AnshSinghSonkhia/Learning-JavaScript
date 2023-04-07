617eb4598a17db51ba029c9baed8e2d740979f96
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-07 17:23:02 +0530

    Update Adding 2 Strings.js

--- Adding 2 Strings.js
+++ Adding 2 Strings.js
@@ -1,1 +1,19 @@
 console.log("add"+"Sum");
+
+
+// Let's create a function scream which will take in a value n and return a string with the letter "a" repeated that many times.
+
+function scream(n) {
+    let str = "";
+
+    for(let i = 0; i<n ; i++){
+        str += "a";
+    }
+
+    return str;
+}
+
+// try Output
+
+scream(5); // "aaaaa"
+scream(10); // "aaaaaaaaaa"
