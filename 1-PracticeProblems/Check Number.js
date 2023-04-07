ae9295f96c63bc6b21c95f72131b67475fa33e44
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-07 19:31:01 +0530

    Create Check Number.js

---
+++ 1-PracticeProblems/Check Number.js
@@ -0,0 +1,14 @@
+/*
+The function checkNumber takes a single argument: a number num
+The function should return the string positive if the number is positive, negative if the number is negative, and zero if the number is zero.
+*/
+
+function checkNumber(num) {
+    if (num < 0) {
+        return "negative";
+    } else if (num > 0) {
+        return "positive";
+    } else {
+        return "zero";
+    }
+}
