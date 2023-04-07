d65e79e407f68587f9cfb30401f0d7b0f64bd550
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2023-04-07 17:15:03 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,24 @@
+// SUMMATION
+
+function summation(n) {
+    let sum = 0;
+
+    for (let i = 1 ; i<=n ; i++) {
+        sum += i
+    }
+
+    return sum;
+}
+
+
+// Factorial
+
+function factorial(n) {
+    let fact = 1;
+
+    for (let i = 1 ; i <= n ; i++){
+        fact = fact * i
+    }
+
+    return fact;
+}
